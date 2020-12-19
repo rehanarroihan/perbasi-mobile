48ec1f0a0149138f0070d672c80307c3b91808b8
rehanarroihan <third2014project@gmail.com>
2020-12-20 00:41:40 +0700

    register page validation

--- lib/ui/pages/auth/register_page.dart
+++ lib/ui/pages/auth/register_page.dart
@@ -4,6 +4,7 @@ import 'package:perbasitlg/ui/widgets/base/box_input.dart';
 import 'package:perbasitlg/ui/widgets/base/button.dart';
 import 'package:perbasitlg/ui/widgets/base/space.dart';
 import 'package:perbasitlg/utils/app_color.dart';
+import 'package:perbasitlg/utils/global_method_helper.dart';
 
 class RegisterPage extends StatefulWidget {
   RegisterPage({Key key}) : super(key: key);
@@ -29,6 +30,21 @@ class _RegisterPageState extends State<RegisterPage> {
     },
   ];
 
+  GlobalKey<FormState> _formKey;
+
+  TextEditingController _nikInput = TextEditingController();
+  TextEditingController _nameInput = TextEditingController();
+  TextEditingController _birthPlaceDateInput = TextEditingController();
+  TextEditingController _birthDateInput = TextEditingController();
+  TextEditingController _passwordInput = TextEditingController();
+  TextEditingController _confirmPasswordInput = TextEditingController();
+
+  @override
+  void initState() {
+    _formKey = GlobalKey<FormState>();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -51,7 +67,9 @@ class _RegisterPageState extends State<RegisterPage> {
             style: AppButtonStyle.secondary,
             text: 'daftar',
             onPressed: () {
+              if (_formKey.currentState.validate()) {
 
+              }
             },
           ),
         ),
@@ -77,31 +95,95 @@ class _RegisterPageState extends State<RegisterPage> {
                 ),
                 Space(height: 10),
                 _roleForm(),
-                BoxInput(
-                  controller: TextEditingController(),
-                  label: 'NIK',
-                ),
                 Space(height: 40),
-                BoxInput(
-                  controller: TextEditingController(),
-                  label: 'Nama',
-                ),
-                Space(height: 40),
-                BoxInput(
-                  controller: TextEditingController(),
-                  label: 'tempat lahir',
-                ),
-                Space(height: 40),
-                BoxInput(
-                  controller: TextEditingController(),
-                  label: 'Kata sandi',
-                  passwordField: true,
-                ),
-                Space(height: 40),
-                BoxInput(
-                  controller: TextEditingController(),
-                  label: 'Ulangi kata sandi',
-                  passwordField: true,
+                Form(
+                  key: _formKey,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      BoxInput(
+                        controller: _nikInput,
+                        label: 'NIK',
+                        keyboardType: TextInputType.number,
+                        validator: (String val) {
+                          if (val.length < 16) {
+                            return 'NIK harus berisikan 16 karakter';
+                          }
+                        },
+                      ),
+                      Space(height: 40),
+                      BoxInput(
+                        controller: _nameInput,
+                        label: 'Nama',
+                        validator: (String val) {
+                          if (val.length < 3) {
+                            return 'nama harus valid';
+                          }
+                        },
+                      ),
+                      Space(height: 40),
+                      BoxInput(
+                        controller: _birthPlaceDateInput,
+                        label: 'Tempat Lahir',
+                        validator: (String val) {
+                          if (val.length < 3) {
+                            return 'tempat lahir harus valid';
+                          }
+                        },
+                      ),
+                      Space(height: 40),
+                      GestureDetector(
+                        onTap: () async {
+                          final DateTime pickedDate = await showDatePicker(
+                            context: context,
+                            initialDate: DateTime.now(),
+                            firstDate: DateTime.now(),
+                            lastDate: DateTime(2025),
+                          );
+                          if (pickedDate != null) {
+                            setState(() {
+                              _birthDateInput.text = pickedDate.toString();
+                            });
+                          } else {
+                            return;
+                          }
+                        },
+                        child: BoxInput(
+                          controller: _birthDateInput,
+                          enabled: false,
+                          label: 'Tanggal Lahir',
+                          validator: (String val) {
+                            if (GlobalMethodHelper.isEmpty(val.length)) {
+                              return 'tanggal lahir harus valid';
+                            }
+                          },
+                        ),
+                      ),
+                      Space(height: 40),
+                      BoxInput(
+                        controller: _passwordInput,
+                        label: 'Kata sandi',
+                        passwordField: true,
+                        validator: (String val) {
+                          if (GlobalMethodHelper.isEmpty(val.length)) {
+                            return 'Password minimal 6 karakter';
+                          }
+                        },
+                      ),
+                      Space(height: 40),
+                      BoxInput(
+                        controller: _confirmPasswordInput,
+                        label: 'Ulangi kata sandi',
+                        passwordField: true,
+                        validator: (String val) {
+                          if (_passwordInput.text != val) {
+                            return 'konfirmasi password tidak valid';
+                          }
+                        },
+                      ),
+                    ],
+                  ),
                 ),
                 Space(height: 32),
               ],
@@ -124,12 +206,12 @@ class _RegisterPageState extends State<RegisterPage> {
               'Daftar sebagai',
               style: TextStyle(
                 fontSize: ScreenUtil().setSp(10),
+                color: Colors.black54
               ),
             ),
           ),
-          Space(height: ScreenUtil().setHeight(8)),
           Container(
-            padding: EdgeInsets.only(bottom: ScreenUtil().setHeight(8)),
+            padding: EdgeInsets.only(bottom: ScreenUtil().setHeight(12)),
             decoration: BoxDecoration(
               border: Border(
                 bottom: BorderSide(
@@ -163,6 +245,7 @@ class _RegisterPageState extends State<RegisterPage> {
     showModalBottomSheet(
       context: context,
       isScrollControlled:true,
+      backgroundColor: Colors.white,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.only(
           topLeft: const Radius.circular(10),
@@ -178,16 +261,15 @@ class _RegisterPageState extends State<RegisterPage> {
               children: [
                 Container(
                   decoration: BoxDecoration(
-                    color: Colors.white,
                     borderRadius: BorderRadius.only(
                       topLeft: const Radius.circular(10),
                       topRight: const Radius.circular(10),
                     ),
                   ),
                   padding: EdgeInsets.only(
-                    left: ScreenUtil().setWidth(12),
-                    right: ScreenUtil().setWidth(12),
-                    top: ScreenUtil().setHeight(12)
+                    left: ScreenUtil().setWidth(20),
+                    right: ScreenUtil().setWidth(20),
+                    top: ScreenUtil().setHeight(20)
                   ),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -215,7 +297,8 @@ class _RegisterPageState extends State<RegisterPage> {
                       _roleOptionChange(_roleList.indexWhere((element) => element == item));
                     }
                   )).toList(),
-                )
+                ),
+                Space(height: 12),
               ],
             );
           }
@@ -239,7 +322,7 @@ class _RegisterPageState extends State<RegisterPage> {
             : Colors.white,
         padding: EdgeInsets.symmetric(
           vertical: ScreenUtil().setHeight(12),
-          horizontal: ScreenUtil().setWidth(12),
+          horizontal: ScreenUtil().setWidth(20),
         ),
         onPressed: () => onClick(),
         child: Row(
--- lib/ui/widgets/base/box_input.dart
+++ lib/ui/widgets/base/box_input.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:perbasitlg/ui/widgets/base/space.dart';
 import 'package:perbasitlg/utils/global_method_helper.dart';
 
 class BoxInput extends StatefulWidget {
   final TextEditingController controller;
   final String label;
   final bool passwordField;
+  final bool enabled;
   final TextInputType keyboardType;
   final Widget suffixWidget;
+  final ValueChanged<String> validator;
 
   BoxInput({
     @required this.controller,
     @required this.label,
+    this.enabled = true,
     this.passwordField = false,
     this.keyboardType = TextInputType.text,
-    this.suffixWidget
+    this.suffixWidget,
+    this.validator
   });
 
   @override
@@ -30,6 +35,8 @@ class _BoxInputState extends State<BoxInput> {
         controller: widget.controller,
         keyboardType: widget.keyboardType,
         obscureText: _obscurePasswordText,
+        enabled: widget.enabled,
+        validator: widget.validator ?? (String args) => null,
         decoration: InputDecoration(
           labelText: widget.label,
           suffixIcon: IconButton(
@@ -51,11 +58,13 @@ class _BoxInputState extends State<BoxInput> {
       return TextFormField(
         controller: widget.controller,
         keyboardType: widget.keyboardType,
+        enabled: widget.enabled,
+        validator: widget.validator ?? (String args) => null,
         decoration: InputDecoration(
           labelText: widget.label,
           suffixIcon: widget.suffixWidget != null
               ? widget.suffixWidget
-              : Container()
+              : Space()
         ),
       );
     }
