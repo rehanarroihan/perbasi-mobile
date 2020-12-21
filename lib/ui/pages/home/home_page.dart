812a1fa30b537322fe6e51999d75b3bf2f38d6f2
rehanarroihan <third2014project@gmail.com>
2020-12-22 06:36:57 +0700

    try integrate widget

--- lib/ui/pages/home/home_page.dart
+++ lib/ui/pages/home/home_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:perbasitlg/cubit/home/home_cubit.dart';
+import 'package:perbasitlg/models/competition_model.dart';
 import 'package:perbasitlg/ui/widgets/base/reactive_refresh_indicator.dart';
 import 'package:perbasitlg/ui/widgets/base/space.dart';
 import 'package:perbasitlg/utils/app_color.dart';
@@ -137,7 +138,7 @@ class _HomePageState extends State<HomePage> {
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
                       Text(
-                        _homeCubit.highlightCompetition.foto,
+                        _homeCubit.highlightCompetition.name,
                         style: TextStyle(
                           color: Colors.white,
                           fontWeight: FontWeight.w600,
@@ -146,7 +147,7 @@ class _HomePageState extends State<HomePage> {
                         overflow: TextOverflow.ellipsis,
                       ),
                       Text(
-                        'Tingkat SMA Se Kab. Tulungagung',
+                        _homeCubit.highlightCompetition.description,
                         style: TextStyle(
                           color: Colors.white,
                           fontWeight: FontWeight.w400,
@@ -170,10 +171,12 @@ class _HomePageState extends State<HomePage> {
       height: ScreenUtil().setHeight(190),
       child: ListView.builder(
         shrinkWrap: true,
-        itemCount: 4,
+        itemCount: _homeCubit.competitions.length,
         padding: EdgeInsets.only(left: ScreenUtil().setWidth(16)),
         scrollDirection: Axis.horizontal,
         itemBuilder: (context, index) {
+          CompetitionModel item = _homeCubit.competitions[index];
+
           return Container(
             width: ScreenUtil().setWidth(230),
             margin: EdgeInsets.only(right: ScreenUtil().setWidth(22)),
@@ -186,15 +189,21 @@ class _HomePageState extends State<HomePage> {
                     width: double.infinity,
                     height: ScreenUtil().setHeight(126),
                     child: CachedNetworkImage(
-                      imageUrl: 'https://scontent-cgk1-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/c0.51.853.853a/s640x640/116793383_155683982795561_3501380845844702714_n.jpg?_nc_ht=scontent-cgk1-1.cdninstagram.com&_nc_cat=105&_nc_ohc=7SgjB_YhcrEAX9PWljX&tp=1&oh=77a60b198456376c48802cbef0023005&oe=6007A236',
+                      imageUrl: item.foto,
                       fit: BoxFit.cover,
                     ),
                   ),
                 ),
                 Space(height: 8),
+                Expanded(
+                  child: Text(
+                    item.name,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
                 Flexible(
                   child: Text(
-                    'Lorem ipsum dolor sisdfsdfsdfsdfsdft amet, consectet ur adipiscing elit. Duis odio sit diam ni sl sed risus nisl, donecsdfsdf',
+                    item.description,
                     overflow: TextOverflow.fade,
                   ),
                 )
