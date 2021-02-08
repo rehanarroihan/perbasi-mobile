ca8fd0b42c43c9115a826aa198a2521e63d13ac3
rehanarroihan <third2014project@gmail.com>
2021-02-09 01:12:31 +0700

    update referee / coach detail profile fix

--- lib/services/profile_service.dart
+++ lib/services/profile_service.dart
@@ -1,14 +1,16 @@
 import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
 
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_uploader/flutter_uploader.dart';
 import 'package:perbasitlg/app.dart';
 import 'package:perbasitlg/models/api_return.dart';
 import 'package:perbasitlg/models/request/profile_coach_request.dart';
 import 'package:perbasitlg/models/request/profile_player_request.dart';
 import 'package:perbasitlg/utils/constant_helper.dart';
 import 'package:perbasitlg/utils/url_constant_helper.dart';
+import 'package:http/http.dart' as http;
 
 class ProfileService {
   Dio _dio = App().dio;
@@ -89,7 +91,7 @@ class ProfileService {
     String role
   }) async {
     try {
-      Response response = await _dio.post(
+      /*Response response = await _dio.post(
         role == ConstantHelper.ROLE_PELATIH
           ? UrlConstantHelper.POST_CHANGE_COACH_PROFILE
           : UrlConstantHelper.POST_CHANGE_REFEREE_PROFILE,
@@ -104,14 +106,61 @@ class ProfileService {
           success: response.data['success'],
           message: response.data['message'],
         );
+      }*/
+
+      String url = role == ConstantHelper.ROLE_PELATIH
+          ? UrlConstantHelper.POST_CHANGE_COACH_PROFILE
+          : UrlConstantHelper.POST_CHANGE_REFEREE_PROFILE;
+
+      var request = http.MultipartRequest(
+          'POST', Uri.parse(
+          App().apiBaseURL + url
+      ));
+      request.fields['email'] = data.email;
+      request.fields['nik'] = data.nik;
+      request.fields['name'] = data.name;
+      request.fields['birth_place'] = data.birthPlace;
+      request.fields['birth_date'] = data.birthDate;
+      request.fields['phone'] = data.phone;
+      request.fields['address'] = data.address;
+      request.fields['licence'] = data.licence;
+      request.fields['licence_number'] = data.licenceNumber;
+      request.fields['licence_from'] = data.licenceFrom;
+      request.fields['licence_active_date'] = data.licenceActiveDate;
+      request.files.add(
+        http.MultipartFile.fromBytes(
+          'licence_file',
+          data.licenceFile.readAsBytesSync(),
+          filename: data.licenceFile.path.split("/").last
+        )
+      );
+      request.files.add(
+        http.MultipartFile.fromBytes(
+          'foto',
+          data.foto.readAsBytesSync(),
+          filename: data.foto.path.split("/").last
+        )
+      );
+      request.headers['Authorization'] = 'Bearer ${App().prefs.getString(ConstantHelper.PREFS_TOKEN_KEY)}';
+
+      var res = await request.send();
+      var responseData = await res.stream.toBytes();
+      var responseString = String.fromCharCodes(responseData);
+      var d = jsonDecode(responseString);
+
+      if (d['success'] == true) {
+        return ApiReturn(
+          success: true,
+          message: d['message'],
+        );
       }
 
-      return ApiReturn(success: false, message: response.data['message']);
+      return ApiReturn(success: false, message: 'Something went wrong');
     } catch (e, stackTrace) {
       print(e);
       return ApiReturn(
         success: false,
-        message: e?.response?.data['message'] ?? 'Something went wrong'
+        message: 'Something went wrong'
       );
     }
   }
